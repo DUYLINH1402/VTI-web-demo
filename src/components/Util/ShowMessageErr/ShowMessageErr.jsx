7396a6fdf3798dd78dddab788de879bd98f60d3e
DUYLINH1402 <145348882+DUYLINH1402@users.noreply.github.com>
2024-07-27 21:14:10 +0900

    Update ShowMessageErr.jsx

--- src/components/Util/ShowMessageErr/ShowMessageErr.jsx
+++ src/components/Util/ShowMessageErr/ShowMessageErr.jsx
@@ -1,9 +1,17 @@
 import './showMessageErr.scss'
+import { Input } from 'antd';
 
-export const ShowMessageErr=()=>{
+export const ShowMessageErr = ({refs, err, name, type = 'text' }) => {
     return (
         <>
-        
+            <Input
+                name={name}
+                status={err ? 'error' : 'success'}
+                ref={refs}
+                placeholder="please input title"
+                type={type}
+            />
+            {err ? <span className="reuqired" >  {err?.message}</span> : null}
         </>
     )
 }
