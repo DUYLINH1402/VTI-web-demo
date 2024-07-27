60206d0fba4e0c6968cbe72b90d418ac3d3459fa
DUYLINH1402 <145348882+DUYLINH1402@users.noreply.github.com>
2024-07-27 13:09:33 +0900

    Update Content.jsx

--- src/components/Home/Content.jsx
+++ src/components/Home/Content.jsx
@@ -1,27 +1,23 @@
-import { SourceContentPage } from '../SourceContentPage/SourceContentPage';
 import './content.scss';
 import { Footer } from './Footer';
+import { CarouselHome } from './HomeContent/CarouselHome';
+import { CourseHome } from './HomeContent/CourseHome';
 
 export const Content = () => {
   return (
     <>
       <article>
-        <div className="content">
+        <CarouselHome />
+        <div className="content-home">
           <img
-            id="img-content-1"
-            src="https://vtiacademy.edu.vn/upload/images/z4670903336619-10cc183d2e4cff8879a8e72511a54c8a.jpg"
-            alt=""
-          />
-          <img
-            id="img-content-2"
+            id="img-content"
             src="https://vtiacademy.edu.vn/upload/images/2048-x-1418(1).jpg"
-            alt=""
+            alt="img-content"
           />
         </div>
-        <div></div>
-        <h1 id="title-cource">Các khóa đào tạo</h1>
       </article>
-      <SourceContentPage />
+        <h1 id="title-cource">Các khóa đào tạo</h1>
+      <CourseHome />
       <Footer />
     </>
   );
