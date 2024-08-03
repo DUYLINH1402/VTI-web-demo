5a880611a5209cf3881f33630e86b094b79c22de
DUYLINH1402 <145348882+DUYLINH1402@users.noreply.github.com>
2024-08-03 21:57:05 +0900

    Update Profile.jsx

--- src/components/Util/Profile/Profile.jsx
+++ src/components/Util/Profile/Profile.jsx
@@ -1,35 +1,22 @@
 import './profile.scss'
-import { getAuth, signOut } from "firebase/auth";
-import { Link, useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
-import { app } from '../../../firebase';
-import user from './../../../image/user.svg';
 import document from './../../../image/document.svg';
 import setting from './../../../image/setting.svg';
 import signout from './../../../image/signout.svg';
-
-
+import { getAuth, signOut } from "firebase/auth";
+import { Link, useNavigate } from 'react-router-dom';
+import { app } from '../../../firebase';
+import user from './../../../image/user.svg';
 
 export const Profile = () => {
-
-
     // if exit authen ...
-    const token = localStorage.getItem('token');
     const navigate = useNavigate();
     const auth = getAuth(app);
-
-    useEffect(() => {
-        if (!token) { // check if token return false
-            navigate('/login'); // Điều hướng về trang Login
-        }
-    }, token);
     const onLogOutPage = () => {
         localStorage.clear(); // delete token from localstorage
         signOut(auth).then(() => {
             // Sign-out successful.
             navigate('/login');
         }).catch((error) => {
-            // An error happened.
             alert(error)
         });
     }
