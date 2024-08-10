32f3ef065b26c624b743f7ed3b550912b2e998d7
DUYLINH1402 <duylinh63b5@gmail.com>
2024-08-10 12:44:11 +0900

    Create Profile.jsx

---
+++ src/components/ProfilePage/Profile/Profile.jsx
@@ -0,0 +1,12 @@
+    
+export const Profile = () => {
+    return (
+        <>
+            <div >
+                Thiết lập các chức năng:  thông tin tài khoản, Hình nền, tên, địa chỉ....
+
+            </div>
+
+        </>
+    )
+}
