7ff61902427cac71c92beaccba340cde8d923044
DUYLINH1402 <duylinh63b5@gmail.com>
2024-08-10 12:44:08 +0900

    Create Setting.jsx

---
+++ src/components/ProfilePage/Setting/Setting.jsx
@@ -0,0 +1,12 @@
+
+export const Setting = () => {
+    return (
+        <>
+            <div >
+                Thiết lập các chức năng:  Cài đăt tài khoản....
+
+            </div>
+
+        </>
+    )
+}
